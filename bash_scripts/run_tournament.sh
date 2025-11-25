a425f2dd0dda862ca4cf3ea4017cdbca65dc354e
hartig <elias.hartig@hhi.fraunhofer.de>
2025-11-25 16:35:05 +0100

    Changes

--- bash_scripts/run_tournament.sh
+++ bash_scripts/run_tournament.sh
@@ -2,14 +2,14 @@
 
 # Simple tournament runner script
 python scripts/tournament.py \
-    --num_openings 200 \
+    --num_openings 10 \
     --model_path "lc0-original.onnx" \
     --in_path "data/eco_openings.pgn" \
     --out_path "results/tournament_games.pgn" \
     --seed 42 \
     --stockfish_binary "Stockfish/src/stockfish" \
-    --temperature 0.0 \
-    --games_per_opening 1
+    --temperature 1.0 \
+    --games_per_opening 5
 #     --use_policy_net_anchor \
 #    --lc0_binary "lc0/build/release/lc0"\
 #    --lc0_weights "768x15x24h-t82-swa-7464000.pb"
